dab71bd181f3cacb80c7463c4c5f8a1b6a95c87d
ismail123123 <149714353+ismail123123@users.noreply.github.com>
2024-03-11 14:26:20 -0400

    aaaa

--- pythonProject/django_auth/accounts/views.py
+++ pythonProject/django_auth/accounts/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import UserChart
+
 from django.views.generic import CreateView
 
 
@@ -15,19 +15,3 @@ class SignUpView(generic.CreateView):
     template_name = "registration/signup.html"
 
 
-def update_chart(request):
-    if request.method == 'POST':
-        label1 = request.POST.get('label1')
-        label2 = request.POST.get('label2')
-        label3 = request.POST.get('label3')
-        value1 = request.POST.get('value1')
-        value2 = request.POST.get('value2')
-        value3 = request.POST.get('value3')
-
-        user_chart = UserChart(user=request.user, label1=label1, label2=label2, label3=label3, value1=value1,
-                               value2=value2, value3=value3)
-        user_chart.save()
-
-        return JsonResponse({'message': 'Chart updated successfully'})
-
-    return render(request, 'update_chart.html')
