7b3a512e8276de979e7d820623fbae6450b0dcdd
abdel <abdellethug460@gmail.com>
2024-05-21 18:18:57 -0400

    1

--- pythonProject/django_auth/accounts/views.py
+++ pythonProject/django_auth/accounts/views.py
@@ -1,148 +1,112 @@
-from django.contrib.auth.models import User
-from django.core.mail import send_mail
-from django.shortcuts import render, redirect
-from django.template.loader import render_to_string
-from django.utils.crypto import get_random_string
+# Import des modules Django nécessaires
 from django.views import generic
-from django.views.generic import CreateView, UpdateView, DeleteView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.shortcuts import render, HttpResponseRedirect
 from django import forms
-from django.shortcuts import render, HttpResponseRedirect
+
 
 from yfinance import Ticker
+from .forms import ProductForm, CompanySearchForm
+from django.shortcuts import redirect, render, get_list_or_404
+from .models import Portfolio
 
-from .models import Portfolio
-from .forms import ProductForm
-from .forms import CompanySearchForm
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib import messages
-from .models import Portfolio
-from .models import Compagny
+
 import yfinance as yf
-from django.shortcuts import get_list_or_404
-
 import numpy as np
-import requests
-"""
-At the command line, only need to run once to install the package via pip:
 
-$ pip install google-generativeai
-"""
 
 import google.generativeai as genai
 
 
+
+# Configuration de l'API de génération
 genai.configure(api_key="AIzaSyA8anLet7yihU2HHOxpp8KP0T2WliU7l_Y")
 
+# Configuration de la génération de texte
 generation_config = {
     "temperature": 1,
     "top_p": 0.95,
     "top_k": 0,
     "max_output_tokens": 8650,
 }
+
+# Paramètres de sécurité pour le modèle de génération
 safety_settings = [
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
 ]
+
+# Initialisation du modèle de génération
 model = genai.GenerativeModel(
     model_name="gemini-1.5-pro-latest",
     generation_config=generation_config,
     safety_settings=safety_settings
 )
 
-
+# Variable globale pour la conversation en cours avec le chatbot
 current_convo = None
+
+# Vue pour gérer les interactions avec le chatbot
 def chatbot(request):
     global current_convo
-
     bot_responses = request.session.get('bot_responses', [])
-
-
     loading = request.GET.get('loading', False)
 
+    # Si le formulaire pour démarrer une nouvelle conversation est soumis
     if request.method == 'POST' and 'new-chat' in request.POST:
         bot_responses.clear()
+        # Démarrage d'une nouvelle conversation avec des messages initiaux
         current_convo = model.start_chat(history=[
-            {
-                "role": "user",
-                "parts": ["Bonjour!"]
-            },
-            {
-                "role": "model",
-                "parts": ["Salut! Comment puis-je vous aider aujourd'hui ?"]
-            }
+            {"role": "user", "parts": ["Bonjour!"]},
+            {"role": "model", "parts": ["Salut! Comment puis-je vous aider aujourd'hui ?"]}
         ])
         request.session['bot_responses'] = bot_responses
         return redirect('chatbot')
 
+    # Si une réponse utilisateur est soumise
     if request.method == 'POST':
         user_input = request.POST.get('user-input')
-
         if user_input:
-
             request.session['loading'] = True
 
+            # Si aucune conversation en cours, démarrer une nouvelle avec des messages initiaux
             if current_convo is None:
-
                 current_convo = model.start_chat(history=[
-                    {
-                        "role": "user",
-                        "parts": ["Bonjour!"]
-                    },
-                    {
-                        "role": "model",
-                        "parts": ["Salut! Comment puis-je vous aider aujourd'hui ?"]
-                    }
+                    {"role": "user", "parts": ["Bonjour!"]},
+                    {"role": "model", "parts": ["Salut! Comment puis-je vous aider aujourd'hui ?"]}
                 ])
 
-
+            # Envoyer le message de l'utilisateur et recevoir la réponse du chatbot
             current_convo.send_message(user_input)
             bot_response = current_convo.last.text
             bot_responses.append(user_input)
             bot_responses.append(bot_response)
-
-
             request.session['bot_responses'] = bot_responses
-
-
             request.session['loading'] = False
 
+    # Affichage de la page du chatbot avec les réponses et l'état de chargement
     return render(request, 'chatbot.html', {'bot_responses': bot_responses, 'loading': loading})
 
+# Formulaire d'inscription avec champ d'email supplémentaire
 class UserCreationFormWithEmail(UserCreationForm):
     email = forms.EmailField(required=True)
 
     def save(self, commit=True):
         user = super(UserCreationFormWithEmail, self).save(commit=False)
         user.email = self.cleaned_data["email"]
-
         if commit:
             user.save()
-
         return user
 
+# Vue d'inscription utilisant le formulaire personnalisé
 class SignUpView(generic.CreateView):
     form_class = UserCreationFormWithEmail
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
-
+# Vue pour gérer le portfolio de l'utilisateur
 def portfolio(request):
     portfolios = Portfolio.objects.filter(user=request.user)
 
@@ -150,51 +114,42 @@ def portfolio(request):
         form = ProductForm(request.POST)
         if form.is_valid():
             form.instance.user = request.user
-
             form.save()
-
             return redirect('portfolio')
     else:
         form = ProductForm()
 
+    # Calculer les informations financières pour chaque élément du portfolio
     for portfolio in portfolios:
-
         company_name = portfolio.company_name
-
         try:
             ticker = yf.Ticker(company_name)
             current_price = ticker.history(period="1d")["Close"].iloc[-1]
             setattr(portfolio, 'current_price', current_price)
-
             info = ticker.info
             company_type = info.get('quoteType', 'Type inconnu')
             setattr(portfolio, 'company_type', company_type)
-
-
         except:
             setattr(portfolio, 'current_price', "Données non disponibles")
             setattr(portfolio, 'company_type', "Type inconnu")
-    context = {
 
+    context = {
         "portfolios": portfolios,
         "form": form,
-
     }
     return render(request, 'portfolio.html', context)
 
-
+# Vue pour supprimer des données du portfolio
 def delete_data(request):
     if request.method == 'POST':
         company_name = request.POST.get('company_name')
         value = request.POST.get('value')
-
         portfolios = get_list_or_404(Portfolio, company_name=company_name, value=value)
         for portfolio in portfolios:
             portfolio.delete()
-
         return redirect('portfolio')
 
-
+# Vue pour rechercher des informations sur une entreprise
 def search_company(request):
     if request.method == 'POST':
         form = CompanySearchForm(request.POST)
@@ -224,34 +179,32 @@ def search_company(request):
                     'signal': row['signal']
                 })
 
-            # algorithme 2
-
+            # Algorithme 2
             ma = 14
             comp_close = history['Close']
             history['returns'] = np.log(comp_close).diff()
             history['ma'] = comp_close.rolling(ma).mean()
             history['ratio'] = comp_close / history['ma']
             percentiles = [5, 95]
-            ratio=history['ratio']
+            ratio = history['ratio']
             p = np.percentile(ratio.dropna(), percentiles)
-
 
             context = {
                 'company': company,
                 'history': history,
                 'macd_data': macd_data,
                 'p': p[0],
-                'p1':p[1],
+                'p1': p[1],
                 'Buy': Buy,
                 'Sell': Sell,
-                'chart_type' : chart_type
+                'chart_type': chart_type
             }
             return render(request, 'search_result.html', context)
     else:
         form = CompanySearchForm()
-        company = "Company not found"
+        company = "Entreprise non trouvée"
         return render(request, 'search_company.html', {'company': company, 'form': form})
 
-from django.shortcuts import render
+# Vue simple pour une page de choix (exemple)
 def choix(request):
-  return  render(request,'choix.html')
+    return render(request, 'choix.html')
--- pythonProject/django_auth/templates/registration/readme.md
+++
@@ -1,6 +0,0 @@
-Finance BMB
-
-
-Notre projet est un site web ou l'utilisateur peut se connecter et a acez a plusieurs fonction telle qu'un portfolio
-qui contient toute les informations sur le type d'invesitessement  que le le rendement les type d'invesstissement le nombre de part acheté,le prix total qu'a couté chacune des actions
-ainsi que la répartition
